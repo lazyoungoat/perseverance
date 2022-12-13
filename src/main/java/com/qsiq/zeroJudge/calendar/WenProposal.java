e26653e90f406a6e1dbcc6d0ff6fee1e6c8cef4e
Nikita <l.r.shiun@gmail.com>
2022-12-13 19:55:55 +0800

    Wen's Proposal to Shan (calendar)

---
+++ src/main/java/com/qsiq/zeroJudge/calendar/WenProposal.java
@@ -0,0 +1,20 @@
+package com.qsiq.zeroJudge.calender;
+
+import java.util.Scanner;
+
+public class WenProposal {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextInt()){
+            int year = Integer.parseInt(scanner.next());
+            if (year%4==0 && year%100!=0){
+                System.out.println("leap year");
+            }else if (year%400==0){
+                System.out.println("leap year");
+            }else{
+                System.out.println("non-leap year");
+            }
+        }
+    }
+}
