8c966b5262a15d80ce56002637141798e278538e
Nikita <l.r.shiun@gmail.com>
2022-12-18 21:36:13 +0800

    geometric sequence only a sequence each time

--- src/main/java/com/qsiq/zeroJudge/loop/EvaAssignment.java
+++ src/main/java/com/qsiq/zeroJudge/loop/EvaAssignment.java
@@ -11,22 +11,41 @@ public class EvaAssignment {
             int inputNumber3 = scanner.nextInt();
             int inputNumber4 = scanner.nextInt();
 
-            int check1 = inputNumber2-inputNumber1;
-            int check2 = inputNumber3-inputNumber2;
-            int check3 = inputNumber4-inputNumber3;
+            int additionCheck1 = inputNumber2-inputNumber1;
+            int additionCheck2 = inputNumber3-inputNumber2;
+            int additionCheck3 = inputNumber4-inputNumber3;
 
-            if((check1==check2)&&(check2==check3)){
+            if((additionCheck1==additionCheck2)&&(additionCheck2==additionCheck3)){
                 for (int i=1;i<6;i++){
                     if (i==1){
                         System.out.println(inputNumber1);
                     }
                     if (i>1){
                         int output;
-                        output = inputNumber1 + check1;
+                        int commonDifference = additionCheck1;
+                        output = inputNumber1 + commonDifference;
                         System.out.println(output);
                         inputNumber1 = output;
                     }
 
+                }
+            }
+
+            int multipleCheck1 = inputNumber2 / inputNumber1;
+            int multipleCheck2 = inputNumber3 / inputNumber2;
+            int multipleCheck3 = inputNumber4 / inputNumber3;
+
+            if ((multipleCheck1==multipleCheck2)&&(multipleCheck2==multipleCheck3)){
+                for (int i=0;i<6;i++){
+                    if (i==1){
+                        System.out.println(inputNumber1);
+                    }
+                    if (i>1){
+                        int commonRatio = inputNumber3 / inputNumber2;
+                        int output = inputNumber1 * commonRatio;
+                        System.out.println(output);
+                        inputNumber1 = output;
+                    }
                 }
             }
         }
