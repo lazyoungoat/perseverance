3332447e20a628c248fff756d82dcb660f077378
Nikita <l.r.shiun@gmail.com>
2022-12-18 21:08:25 +0800

    only arithmetic progression and only a single sequence each time

--- src/main/java/com/qsiq/zeroJudge/loop/EvaAssignment.java
+++ src/main/java/com/qsiq/zeroJudge/loop/EvaAssignment.java
@@ -5,32 +5,28 @@ import java.util.Scanner;
 public class EvaAssignment {
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
-//        String input = scanner.next();
         while(scanner.hasNextInt()){
-            int i=0;
-            int inputNumber1 = Integer.parseInt(scanner.next());
-            i++;
-//            int inputNumber2 = Integer.parseInt(input);
-            int inputNumber2 = Integer.parseInt(scanner.next());
-            i++;
-//            int inputNumber3 = Integer.parseInt(input);
-            int inputNumber3 = Integer.parseInt(scanner.next());
-            i++;
-//            int inputNumber4 = Integer.parseInt(input);
-            int inputNumber4 = Integer.parseInt(scanner.next());
-            i++;
-//            if (inputNumber1<105 && inputNumber2<105 && inputNumber3<105 && inputNumber4<105){
-//            };
-            if (i==4){
-                break;
-            }
-            int check1 = inputNumber1-inputNumber2;
-            int check2 = inputNumber2-inputNumber3;
-            int check3 = inputNumber3-inputNumber4;
+            int inputNumber1 = scanner.nextInt();
+            int inputNumber2 = scanner.nextInt();
+            int inputNumber3 = scanner.nextInt();
+            int inputNumber4 = scanner.nextInt();
+
+            int check1 = inputNumber2-inputNumber1;
+            int check2 = inputNumber3-inputNumber2;
+            int check3 = inputNumber4-inputNumber3;
 
             if((check1==check2)&&(check2==check3)){
-                for (i=0;i<5;i++){
-                    System.out.println(i+=check1);
+                for (int i=1;i<6;i++){
+                    if (i==1){
+                        System.out.println(inputNumber1);
+                    }
+                    if (i>1){
+                        int output;
+                        output = inputNumber1 + check1;
+                        System.out.println(output);
+                        inputNumber1 = output;
+                    }
+
                 }
             }
         }
