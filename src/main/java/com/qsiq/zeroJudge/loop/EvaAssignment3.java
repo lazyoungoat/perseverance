b82a84c7f116756aa63f2ab4a66f8f0cc1d66e8e
Nikita <l.r.shiun@gmail.com>
2023-01-13 12:10:57 +0800

    Eva assignment reference Hank Yeh's method

---
+++ src/main/java/com/qsiq/zeroJudge/loop/EvaAssignment3.java
@@ -0,0 +1,27 @@
+package com.qsiq.zeroJudge.loop;
+
+import java.util.Scanner;
+
+public class EvaAssignment3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int loop = scanner.nextInt();
+
+        for (int i=0;i<loop;i++){
+            Scanner scanner1 = new Scanner(System.in);
+            Scanner scanner2 = new Scanner(System.in);
+            Scanner scanner3 = new Scanner(System.in);
+            Scanner scanner4 = new Scanner(System.in);
+            int num1 = scanner1.nextInt();
+            int num2 = scanner2.nextInt();
+            int num3 = scanner3.nextInt();
+            int num4 = scanner4.nextInt();
+            if ((num3-num2)==(num2-num1)){
+                System.out.println(num1 + " " + num2 + " " + num3 + " " + num4 + " " + (num4+(num2-num1)));
+            }else {
+                System.out.println(num1 + " " + num2 + " " + num3 + " " + num4 + " " + (num4*(num2/num1)));
+            }
+        }
+
+    }
+}
