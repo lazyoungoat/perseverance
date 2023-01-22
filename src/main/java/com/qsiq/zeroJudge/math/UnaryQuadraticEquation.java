5d9e3da19d70b66e63e3ae66e28e4536cb00ec06
Nikita <l.r.shiun@gmail.com>
2023-01-23 01:57:57 +0800

    Eva's assignment zerojudge RE(code:1)

---
+++ src/main/java/com/qsiq/zeroJudge/math/UnaryQuadraticEquation.java
@@ -0,0 +1,17 @@
+package com.qsiq.zeroJudge.math;
+
+import java.util.Scanner;
+
+public class UnaryQuadraticEquation {
+    public static void main(String[] args){
+        int a, b, c, x1, x2;
+        a = 0;
+        b = 0;
+        c = 0;
+        x1 = 0;
+        x2 = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
