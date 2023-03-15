8c9f52446fd83940e5f5d767f0209aec22762af7
Nikita <l.r.shiun@gmail.com>
2023-03-16 01:01:29 +0800

    leetcode solution 1512 easy

--- src/main/java/leetcode/easy/Solution.java
+++ src/main/java/leetcode/easy/Solution.java
@@ -1,9 +1,6 @@
 package leetcode.easy;
 
 public class Solution {
-    public static void main(String[] args){
-        for (int i = 5; i > -1 ; i--) {
-            System.out.println(i);
-        }
+    public static void main(String[] args) {
     }
 }
