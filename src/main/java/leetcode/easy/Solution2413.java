1b10116d4bde173faf9b6c7a48862dd45614c00e
Nikita <l.r.shiun@gmail.com>
2023-03-23 20:25:25 +0800

    leetcode solution 2413 easy

---
+++ src/main/java/leetcode/easy/Solution2413.java
@@ -0,0 +1,11 @@
+package leetcode.easy;
+
+public class Solution2413 {
+    public int smallestEvenMultiple(int n) {
+        if (n % 2 == 0) {
+            return n;
+        } else {
+            return n * 2;
+        }
+    }
+}
