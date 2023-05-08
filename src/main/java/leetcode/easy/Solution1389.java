115786743685ddf5d9ae9ea838c9e6b1a0a68a42
Nikita <l.r.shiun@gmail.com>
2023-05-09 01:12:01 +0800

    leetcode easy 1389 solution

--- src/main/java/leetcode/easy/Solution1389.java
+++ src/main/java/leetcode/easy/Solution1389.java
@@ -1,18 +1,17 @@
 package leetcode.easy;
 
+import java.util.ArrayList;
+
 public class Solution1389 {
     public int[] createTargetArray(int[] nums, int[] index) {
         int[] target = new int[nums.length];
+        ArrayList<Integer> arr = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            if (target[index[i]] == 0) {
-                target[index[i]] = nums[i];
-            }else {
-                while(target[index[i]++]!=0){
-                    int tmp = target[index[i]++];
-                    target[index[i]++] = target[index[i]];
-                    target[index[i]+=2] = tmp;
-                }
-            }
+            arr.add(index[i], nums[i]);
+        }
+
+        for (int i = 0; i < target.length; i++) {
+            target[i] = arr.get(i);
         }
         return target;
     }
